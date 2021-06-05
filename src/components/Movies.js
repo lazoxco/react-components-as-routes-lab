e88c7be8a9d746eb87ffdb07b3e4b8b43f5addf5
github username <github email address>
2021-06-05 12:35:48 -0400

    Built Movies and Directors components.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,27 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  
+  const renderMovies = () => (
+    movies.map((movie, index) => (
+      <div key={index}>
+        <h3>{movie.title}</h3>
+        <p>Time: {movie.time}</p>
+        <p>Genres:</p>
+        <ul>
+          {movie.genres.map((genre, index) => (
+            <li key={index}>{genre}</li>
+          ))}
+        </ul>
+        <hr/>
+      </div>
+    ))
+  )
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
