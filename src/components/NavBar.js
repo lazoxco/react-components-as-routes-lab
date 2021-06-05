b9b0c3daa3e675bad6c1bc8dab4f727897d832f4
github username <github email address>
2021-06-05 12:01:34 -0400

    Added NavLinks to NavBar component.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink style={link} activeStyle={{background: 'darkblue'}} to="/" exact >Home</NavLink>
+      <NavLink style={link} activeStyle={{background: 'darkblue'}} to="/movies" exact >Movies</NavLink>
+      <NavLink style={link} activeStyle={{background: 'darkblue'}} to="/directors" exact >Directors</NavLink>
+      <NavLink style={link} activeStyle={{background: 'darkblue'}} to="/actors" exact >Actors</NavLink>
     </div>
   );
 };
